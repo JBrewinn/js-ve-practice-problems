2a90a0fee4bd012e88860e2160d62ae687ad7715
JBrewinn <jyow97@gmail.com>
2022-01-25 20:44:31 -0600

    updated practice

--- practice.js
+++ practice.js
@@ -1,14 +1,8 @@
-var name = prompt("What is your name?")
-var city = prompt("In which city or town do you live?");
-var age = prompt("How old are you?");
-var pets = prompt("How many pets do you have?");
-var food = prompt("What is your favorite food?");
- 
-var out = "This is my friend " + name;
-
-out += "<br>" +  name + " lives in " + city;
-out += "<br>" +  name + " is " + age + " years old";
-out += "<br>" + name + " has " + pets + " pets";
-out += "<br>" +  name + " likes to eat " + food;
-
-document.getElementById("output").innerHTML = out;
+name1 = "Tom";
+name2 = "Bob";
+name3 = "Mary";
+name4 = "Caitlyn";
+name5 = "Red";
+name6 = "Douglas";
+name7 = "Judy";
+name8 = "Susan";
