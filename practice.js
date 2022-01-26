910f81e17c844dee6f22b4d753855ae83ad36845
JBrewinn <jyow97@gmail.com>
2022-01-26 17:50:06 -0600

    added css

--- practice.js
+++ practice.js
@@ -1,13 +1,3 @@
-var day = today.getDay();
-var dayList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-var hour = today.getHour();
-var minute = today.getMinutes();
-var second = today.getSeconds();
-
-
-
-var out = "Today is: " + dayList[day] + ".";
-out + "<br>Current time is: " + hour + " : " + minute + " : " + second + "."
-
-document.getElementById("output").innerHTML = out;
+function printPage() {
+    window.print();
+}
