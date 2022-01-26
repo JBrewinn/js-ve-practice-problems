faf15265ec3ec7d83443519b7f5012d57d0f9a11
JBrewinn <jyow97@gmail.com>
2022-01-25 20:41:31 -0600

    Practice #2

--- practice.js
+++ practice.js
@@ -1,5 +1,14 @@
-function howManySeconds(hours) {
-	return (hours * 60) * 60;
-} 
+var name = prompt("What is your name?")
+var city = prompt("In which city or town do you live?");
+var age = prompt("How old are you?");
+var pets = prompt("How many pets do you have?");
+var food = prompt("What is your favorite food?");
+ 
+var out = "This is my friend " + name;
 
-console.log(howManySeconds(1));
+out += "<br>" +  name + " lives in " + city;
+out += "<br>" +  name + " is " + age + " years old";
+out += "<br>" + name + " has " + pets + " pets";
+out += "<br>" +  name + " likes to eat " + food;
+
+document.getElementById("output").innerHTML = out;
