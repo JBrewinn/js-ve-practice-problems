e2fee27ee0418f85cb0b43d855795337a7b53b8c
JBrewinn <jyow97@gmail.com>
2022-01-26 18:01:44 -0600

    Finished practice

--- practice.js
+++ practice.js
@@ -1,3 +1,11 @@
-function printPage() {
-    window.print();
-}
+var today = new Date;
+var month = today.getMonth();
+var monthList = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+var day = today.getDate();
+var year = today.getFullYear();
+
+out = "Today's date is " + monthList[month] + "/" + day + "/" + year + ".";
+
+console.log(out);
+
+document.getElementById("output").innerHTML = out;
