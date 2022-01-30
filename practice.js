699437f1da267d82eea5b66851a164405f904a88
JBrewinn <jyow97@gmail.com>
2022-01-29 20:48:42 -0600

    practice 1.29

--- practice.js
+++ practice.js
@@ -1,10 +1,14 @@
-for (let num = 0; num <= 100; num++) {
-    const gnum = array[num];
-    if (gnum % 15 === 0) {
-        console.log("FizzBuzz");
-    } else if (gnum % 3 === 0) {
-        console.log("Fizz");
-    } else if (gnum % 5 === 0) {
-        console.log("Buzz");
+function FizzBuzz(num) {
+    for (let num = 0; num <= 100; num++) {
+        const gnum = array[num];
+        if (gnum % 15 === 0) {
+            console.log("FizzBuzz");
+        } else if (gnum % 3 === 0) {
+            console.log("Fizz");
+        } else if (gnum % 5 === 0) {
+            console.log("Buzz");
+        }
     }
 }
+
+console.log(FizzBuzz(0));
