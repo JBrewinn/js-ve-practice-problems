c05a04efb4e326b1e474fa77a59fa6fd381acf68
JBrewinn <jyow97@gmail.com>
2022-01-29 20:58:04 -0600

    Finished Very Easy Problems

--- practice.js
+++ practice.js
@@ -1,14 +1,24 @@
-function FizzBuzz(num) {
-    for (let num = 0; num <= 100; num++) {
-        const gnum = array[num];
-        if (gnum % 15 === 0) {
-            console.log("FizzBuzz");
-        } else if (gnum % 3 === 0) {
-            console.log("Fizz");
-        } else if (gnum % 5 === 0) {
-            console.log("Buzz");
-        }
-    }
+// Write a function that converts hours into seconds.
+
+function howManySeconds(num) {
+    return num *60 *60;
 }
 
-console.log(FizzBuzz(0));
+// console.log(howManySeconds(10));
+
+// Create a function that takes length and width and finds the perimeter of a rectangle.
+
+function findPerimeter(a, b) {
+    return (a * 2) + (b * 2);
+}
+
+// console.log(findPerimeter(20, 10));
+
+// A student learning JavaScript was trying to make a function. His code should concatenate a passed string name with string "Edabit" and store it in a variable called result. He needs your help to fix this code.
+
+function nameString(name) {
+    let result = name + "Edabit";
+    return result;
+}
+
+console.log(nameString("javaScript"));
