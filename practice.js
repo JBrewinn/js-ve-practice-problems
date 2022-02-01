dd4aef964706b2158e5a6a6af3fd71b96540e2e1
JBrewinn <jyow97@gmail.com>
2022-01-31 20:56:44 -0600

    infinite loop problem

--- practice.js
+++ practice.js
@@ -23,3 +23,16 @@ function nameString(name) {
 
 // console.log(nameString("javaScript"));
 
+// Mubashir created an infinite loop! Help him by fixing the code in the code tab to pass this challenge. Look at the examples below to get an idea of what the function should do.
+
+function printArray(number) {
+    var newArray = [];
+  
+    for(let i = 1; i <= number; i++) {
+      newArray.push(i);
+    }
+  
+    return newArray;
+  }
+
+  console.log(printArray(8));
