bd37fbc9e7625d21e35e7b49a8f8af33d3e588fa
JBrewinn <jyow97@gmail.com>
2022-02-02 14:46:22 -0600

    BOD 2.2.2022

--- practice.js
+++ practice.js
@@ -126,4 +126,14 @@ function greeting(name) {
 
 // console.log(greeting("Mubashir"));
 
+// Create a function that takes two arguments. Both arguments are integers, a and b. Return true if one of them is 10 or if their sum is 10.
 
+function makesTen(a, b) {
+	if (a == 10 || b == 10) {
+		return true;
+	} else if ((a + b) == 10) {
+		return true;
+	} return false;
+}
+
+console.log(makesTen(2, 1));
