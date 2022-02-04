42dabdf73f59d0f5d3f817ec8ba0e3d896e215d5
JBrewinn <jyow97@gmail.com>
2022-02-04 15:44:06 -0600

    pause

--- practice.js
+++ practice.js
@@ -310,5 +310,13 @@ function reverse(arr) {
 	return arr.reverse();
 }
 
-console.log(reverse([1, 2, 3, 4]));
-console.log(reverse([]));
+
+// Write a function that checks whether a person can watch an MA15+ rated movie. One of the following two conditions is required for admittance:
+
+// The person is at least 15 years old.
+// They have parental supervision.
+// The function accepts two parameters, age and isSupervised. Return a boolean.
+
+function acceptIntoMovie(age, isSupervised) {
+	
+}
