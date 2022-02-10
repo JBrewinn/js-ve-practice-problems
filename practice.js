be064636960a6d35fc7699ed9ed6da7497dab41d
JBrewinn <jyow97@gmail.com>
2022-02-10 13:04:43 -0600

    progress 13:04 2.10.2022

--- practice.js
+++ practice.js
@@ -727,3 +727,14 @@ function integerBoolean(num) {
 // console.log(integerBoolean("100101"));
 
 
+// You are given an array of dates in the format Dec 11 and a monthin the format Dec as arguments. Each date represents a video that was uploaded on that day. Return the number of uploads for a given month.
+
+var tot = 0;
+
+function uploadCount(dates, month) {
+	for (let i = 0; i < dates.length; i++) {
+		if (dates[i].startsWith(month)) {
+			tot += 1;
+		}
+	}  return tot;
+}
