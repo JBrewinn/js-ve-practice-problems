a53f98fcd5ffa381f5a73604d970051157bd46ae
JBrewinn <jyow97@gmail.com>
2022-03-22 12:20:10 -0500

    return to practice

--- practice.js
+++ practice.js
@@ -811,12 +811,16 @@ function sumDigits(n) {
 // console.log(sumDigits(9102948));
 
 
-// Create a function that returns the average of a list composed of letters. First, find the number of the letter in the alphabet in order to find the average of the list.
+// Create a function that takes two numbers num1, num2, and an array arr and returns an array containing all the numbers in arr greater than num1 and less than num2.
 
-new Object = [
-	a
-]
+function arrBetween(num1, num2, arr) {
+	var result = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] > num1 && arr[i] < num2) {
+			result.push(arr[i]);
+		}
+	} return result;
+}
 
-function averageIndex(arr) {
-	
-}	
+// console.log(arrBetween(3, 8, [1, 5, 95, 0, 4, 7]));
+// console.log(arrBetween(1, 10, [1, 10, 25, 8, 11, 6]));
